74558be3e9d3f4dedc3f8c5f4d70b044fa822f38
13146637707 <9373289+ZiyuLiu07@user.noreply.gitee.com>
2022-04-28 14:35:54 +0800

    delete useless code

--- shader/shader.h
+++ shader/shader.h
@@ -26,20 +26,7 @@ color shader::render(const ray &r, const hittable &world, int depth, color backg
     if (depth <= 0){
         return color(0,0,0);
     }
-
-    /*if (world.hit(r, 0.001, infinity, rec)) {
-        ray scattered;
-        color attenuation;
-        if(rec.mat_ptr -> scatter(r, rec, attenuation, scattered)){
-            return attenuation * render(scattered, world, depth - 1);
-        }
-        return color(1, 1, 1);
-    }
-
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0-t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);*/
-    // If the ray hits nothing, return the background color.
+    
     if (!world.hit(r, 0.001, infinity, rec))
         return background;
 
